f6b5299e953d82322060a239ff9d961108a208ca
Pablo <superpato1234@hotmail.com>
2021-08-19 23:07:15 -0500

    ciclo de eventos node

---
+++ introduccion/hello-world/app1.js
@@ -0,0 +1,6 @@
+const Saludar = (nombre) => {
+    return `Hola a todos perros, soy ${nombre}`;
+}
+
+
+console.log(Saludar('pablito mix'));
---
+++ introduccion/hello-world/app2.js
@@ -0,0 +1,15 @@
+console.log("inicio de programa");
+
+setTimeout( ()=>{
+    console.log("primer timeout")
+},3000)
+
+setTimeout( ()=>{
+    console.log("segundo timeout")
+},0)
+
+setTimeout( ()=>{
+    console.log("tercer timeout")
+},0)
+
+console.log("fin de programa")
